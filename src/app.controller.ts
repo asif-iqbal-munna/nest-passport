16208ee779ef3bc7607f1b79fd0cecf9952e5749
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2022-01-09 18:09:41 +0600

    nest and passport js practiced

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
