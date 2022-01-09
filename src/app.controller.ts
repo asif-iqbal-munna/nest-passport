c6095343784ad55417a12396206d56d52ec57020
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2022-01-10 01:38:14 +0600

    passport jwt practiced

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('/auth/login')
+  @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
